3b86a71cc09b03b4b2265e245950f0a6d74ecbfa
RicardoZanandrea <ricardinhodev@gmail.com>
2025-09-10 23:10:10 -0300

    Greeting service: Different languages based on the url parameter

---
+++ src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
@@ -0,0 +1,51 @@
+package br.edu.atitus.greeting_service.controllers;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// import br.edu.atitus.greeting_service.configs.GreetingConfig;
+
+@RestController
+@RequestMapping("greeting-service")
+public class GreetingController {
+
+    // private final GreetingConfig greetingConfig;
+
+    // public GreetingController(GreetingConfig greetingConfig) {
+    //     super();
+    //     this.greetingConfig = greetingConfig;
+    // }
+    
+    private final MessageSource messageSource;
+
+    public GreetingController(MessageSource messageSource) {
+        System.out.println(".()");
+        this.messageSource = messageSource;
+    }
+
+    
+    @GetMapping({"", "/", "/{namePath}"})
+    public ResponseEntity<String> getGreetingService(
+        @RequestParam(required = false) String name,
+        @PathVariable(required = false) String namePath,
+        @RequestParam(defaultValue = "en") String lang) {
+        
+        Locale locale = Locale.forLanguageTag(lang);
+
+        String greeting = messageSource.getMessage("greeting.text", null, locale);
+        String defaultName = messageSource.getMessage("default.name", null, locale);
+
+        if (name == null)
+            name = namePath != null ? namePath : defaultName;
+        
+        String textReturn = String.format("%s, %s!!", greeting, name);
+        return ResponseEntity.ok(textReturn);
+    }
+}
